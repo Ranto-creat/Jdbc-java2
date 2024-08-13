e0d93fd1a737e7a76f8a0bfb97c29a51af61b161
Ranto-Creat <hei.ranto.2@gmail.com>
2024-08-13 22:13:40 +0300

    feat: add the class java for all attribut to this class

---
+++ Student.java
@@ -0,0 +1,14 @@
+package Revision;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Student {
+  private int id;
+  private String name;
+  private int age;
+  private String email;
+}
